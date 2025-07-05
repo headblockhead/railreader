7d734b2832cc73325a1494f455b8f8f0f3b2454a
Edward Hesketh <inbox@edwardh.dev>
2025-07-05 20:00:46 +0100

    feat: Reading from the Darwin datastream

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.24.4
 require github.com/segmentio/kafka-go v0.4.48
 
 require (
-	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/alecthomas/kong v1.12.0 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/pierrec/lz4/v4 v4.1.15 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,14 +1,139 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"errors"
+	"log/slog"
+	"os"
+	"os/signal"
+	"runtime"
+	"sync"
+	"syscall"
+
+	"github.com/alecthomas/kong"
 	"github.com/segmentio/kafka-go"
 )
 
-func main() {
-	conn, err := kafka.Dial("tcp", "pkc-z3p1v0.europe-west2.gcp.confluent.cloud:9092")
-	if err != nil {
-		panic(err.Error())
+type CLI struct {
+	Serve ServeCommand `cmd:"serve"`
+}
+
+type ServeCommand struct {
+	DarwinServer   string `env:"DARWIN_SERVER" required:"" name:"serv"`
+	DarwinGroupID  string `env:"DARWIN_GROUPID" required:"" name:"gid"`
+	DarwinUsername string `env:"DARWIN_USERNAME" required:"" name:"user"`
+	DarwinPassword string `env:"DARWIN_PASSWORD" required:"" name:"pass"`
+
+	LogLevel   string `default:"info" enum:"debug,info,warn,error" help:"Set the logging level."`
+	JSONOutput bool   `default:"false" help:"Output logs in JSON format instead of text."`
+}
+
+func getLogger(logLevel string, JSONOutput bool) *slog.Logger {
+	var level slog.Level
+	switch logLevel {
+	case "debug":
+		level = slog.LevelDebug
+	case "info":
+		level = slog.LevelInfo
+	case "warn":
+		level = slog.LevelWarn
+	case "error":
+		level = slog.LevelError
 	}
-	defer conn.Close()
+	var log *slog.Logger
+	if JSONOutput {
+		log = slog.New(slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{
+			Level: level,
+		}))
+	} else {
+		log = slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
+			Level: level,
+		}))
+	}
+	return log
+}
+
+var currentlyTerminating bool = false
+
+func main() {
+	var cli CLI
+	kctx := kong.Parse(&cli, kong.UsageOnError(), kong.BindTo(context.Background(), (*context.Context)(nil)))
+	if err := kctx.Run(); err != nil {
+		log := getLogger("error", true)
+		log.Error("error", slog.Any("error", err))
+		os.Exit(1)
+	}
+}
+
+func (c ServeCommand) Run(connectionContext context.Context) error {
+	log := getLogger(c.LogLevel, c.JSONOutput)
+	connectionContext, connectionCancel := context.WithCancel(context.Background())
+	fetcherContext, fetcherCancel := context.WithCancel(context.Background())
+
+	signalchan := make(chan os.Signal, 1)
+	signal.Notify(signalchan, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		for {
+			<-signalchan
+			if !currentlyTerminating {
+				log.Info("SIGINT/SIGTERM recieved, stopping gracefully...")
+				currentlyTerminating = true
+				fetcherCancel()
+			} else {
+				log.Info("recieved multiple SIGINT/SIGTERM signals, exiting immediately")
+				os.Exit(130)
+			}
+		}
+	}()
+
+	dc := NewDarwinConnection(connectionContext, fetcherContext, log.With(slog.String("source", "darwin")), c.DarwinServer, c.DarwinGroupID, c.DarwinUsername, c.DarwinPassword)
+	darwinKafkaMessages := make(chan kafka.Message, 64)
+
+	var darwinProcessorGroup sync.WaitGroup
+
+	for range runtime.NumCPU() {
+		darwinProcessorGroup.Add(1)
+		go func() {
+			defer darwinProcessorGroup.Done()
+			processKafkaMessages(log, dc, darwinKafkaMessages)
+		}()
+	}
+
+	FetchKafkaMessages(log, dc, darwinKafkaMessages)
+
+	close(darwinKafkaMessages)
+	darwinProcessorGroup.Wait()
+	connectionCancel()
+	dc.Close()
+
+	return nil
+}
+
+func FetchKafkaMessages(log *slog.Logger, dc *DarwinConnection, darwinKafkaMessages chan kafka.Message) {
+	for {
+		msg, err := dc.FetchKafkaMessage()
+		if err != nil {
+			if errors.Is(err, context.Canceled) {
+				log.Debug("context canceled while fetching Kafka message")
+				break
+			}
+			log.Error("error fetching Kafka message", slog.Any("error", err))
+			continue
+		}
+		darwinKafkaMessages <- msg
+	}
+}
+
+func processKafkaMessages(log *slog.Logger, dc *DarwinConnection, darwinKafkaMessages chan kafka.Message) {
+	for msg := range darwinKafkaMessages {
+		if currentlyTerminating {
+			log.Debug("program terminating, processing remaining Kafka messages", slog.Int("remaining", len(darwinKafkaMessages)))
+		}
+		err := dc.ProcessKafkaMessage(msg)
+		if err != nil {
+			log.Error("error processing Kafka message", slog.Any("error", err))
+			continue
+		}
+	}
 }
