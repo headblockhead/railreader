35bb95cbf883b0ad75ec5fe15247f7ae28d4cc02
Edward Hesketh <inbox@edwardh.dev>
2025-12-04 14:36:06 +0000

    wip: train order

--- ingesters/darwin/interpreter/trainorder.go
+++ ingesters/darwin/interpreter/trainorder.go
@@ -1,7 +1,58 @@
 package interpreter
 
-import "github.com/headblockhead/railreader/ingesters/darwin/unmarshaller"
+import (
+	"github.com/google/uuid"
+	"github.com/headblockhead/railreader/ingesters/darwin/unmarshaller"
+)
 
 func (u *UnitOfWork) interpretTrainOrder(trainOrder unmarshaller.TrainOrder) error {
 	return nil
+}
+
+type trainOrderRecord struct {
+	ID uuid.UUID
+
+	MessageID string
+
+	LocationID string
+	Platform   string
+
+	ClearOrder bool
+
+	FirstServiceRID                  string
+	FirstServiceHeadcode             string
+	FirstServiceWorkingArrivalTime   *string
+	FirstServiceWorkingPassingTime   *string
+	FirstServiceWorkingDepartureTime *string
+	FirstServicePublicArrivalTime    *string
+	FirstServicePublicDepartureTime  *string
+
+	SecondServiceRID                  string
+	SecondServiceHeadcode             string
+	SecondServiceWorkingArrivalTime   *string
+	SecondServiceWorkingPassingTime   *string
+	SecondServiceWorkingDepartureTime *string
+	SecondServicePublicArrivalTime    *string
+	SecondServicePublicDepartureTime  *string
+
+	ThirdServiceRID                  *string
+	ThirdServiceHeadcode             *string
+	ThirdServiceWorkingArrivalTime   *string
+	ThirdServiceWorkingPassingTime   *string
+	ThirdServiceWorkingDepartureTime *string
+	ThirdServicePublicArrivalTime    *string
+	ThirdServicePublicDepartureTime  *string
+}
+
+func (u *UnitOfWork) trainOrderToRecord(trainOrder unmarshaller.TrainOrder) (trainOrderRecord, error) {
+	var record trainOrderRecord
+	record.ID = uuid.New()
+	record.MessageID = *u.messageID
+	record.LocationID = trainOrder.TIPLOC
+	record.Platform = trainOrder.Platform
+	record.ClearOrder = bool(trainOrder.ClearOrder)
+	if trainOrder.Services == nil {
+		return record, nil
+	}
+	// TODO
 }
